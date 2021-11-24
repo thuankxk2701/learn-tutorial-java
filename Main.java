b273ddb12a9ae387ca7cb7ee604afca26a2cb7ac
Thuan <ttctde@gmail.com>
2021-11-24 20:35:54 +0700

    init commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+	String name =JOptionPane.showInputDialog("key");
+    JOptionPane.showMessageDialog(null,"hello"+name);
+    }
+}
